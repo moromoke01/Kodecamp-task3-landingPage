a83b1bb6ac780681734f1c2acb0ffb78ec0973b7
moromoke01 <56760496+moromoke01@users.noreply.github.com>
2024-06-12 11:51:38 +0100

    Project.jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -4,6 +4,7 @@ import projectImg2 from '../assets/project-img2.png';
 import projectImg3 from '../assets/project-img3.png';
 import projectImg4 from '../assets/project-img4.png';
 import { IoIosArrowRoundForward } from "react-icons/io";
+import './Project.css'; // Import CSS file for styling
 
 const Project = () => {
   return (
@@ -12,7 +13,10 @@ const Project = () => {
       <div className='project-img-grid'>
         <div className="FirstProjectImg">
           <img src={projectImg0} alt="projectImg0" />
-          <h2 className="content">Sample Project</h2>
+          <div className="project-info">
+            <h2 className="content">Sample Project</h2>
+            <p className="description">This is a description of the sample project. You can add more details here.</p>
+          </div>
         </div>
         <img src={projectImg1} alt="projectImg1" />
         <img src={projectImg3} alt="projectImg3" />
