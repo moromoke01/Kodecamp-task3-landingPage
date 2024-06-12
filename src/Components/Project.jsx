8a8810061cbbf1e20a2d9c963fb224d2ca400406
moromoke01 <56760496+moromoke01@users.noreply.github.com>
2024-06-12 12:06:16 +0100

    Project.jsx

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -13,7 +13,6 @@ const Project = () => {
       <div className='project-img-grid'>
           <div className="FirstProjectImg"><img src={projectImg0} alt="projectImg0" /> <h2 className="content">Sample Project</h2></div>
           <img src={projectImg1} alt="projectImg1" />
-          <img src={projectImg3} alt="projectImg3" />
           <img src={projectImg2} alt="projectImg2" />
           <img src={projectImg3} alt="projectImg3" />
           <img src={projectImg4} alt="projectImg" />
